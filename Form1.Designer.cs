0596a726c6f7da636b7dc68c82508cab94f565b3
TatsianaMaltseva <54571357+TatsianaMaltseva@users.noreply.github.com>
2021-04-08 22:48:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Anal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.White;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripSeparator2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1159, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(96, 24);
+            this.toolStripButton1.Text = "Width signal";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(218, 24);
+            this.toolStripButton2.Text = "Photoconductivity dependence";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.cartesianChart1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 30);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(925, 641);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox1.Location = new System.Drawing.Point(1034, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(931, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter alpha";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button1.Location = new System.Drawing.Point(1034, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(931, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "L";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(931, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "hv";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(931, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "U";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(931, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "U/I";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(931, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "G";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(931, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(193, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Valid values are [1500; 3300]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1159, 668);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Photoconductivity";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Anal
+{
+    public partial class Form1 : Form
+    {
+        class Data1Graph
+        {
+            public double values { get; set; }
+            public double date { get; set; }
+        }
+        class Data2Graph
+        {
+            public double values { get; set; }
+            public double date { get; set; }
+        }
+
+        List<Data1Graph> datas1;
+        List<Data2Graph> datas2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            datas1 = new List<Data1Graph>() // Массив с U от d
+            {
+                new Data1Graph(){ values = 77.9, date = 0.1 },
+                new Data1Graph(){ values = 195.6, date = 0.2 },
+                new Data1Graph(){ values = 196.1, date = 0.3 },
+                new Data1Graph(){ values = 196.4, date = 0.4 },
+                new Data1Graph(){ values = 196.7, date = 0.5 },
+                new Data1Graph(){ values = 196.9, date = 0.6 },
+                new Data1Graph(){ values = 197.0, date = 0.7 },
+                new Data1Graph(){ values = 197.1, date = 0.8 },
+                new Data1Graph(){ values = 197.2, date = 0.9 },
+                new Data1Graph(){ values = 197.3, date = 1.0 },
+                new Data1Graph(){ values = 197.4, date = 1.1 },
+                new Data1Graph(){ values = 197.4, date = 1.2 },
+                new Data1Graph(){ values = 197.5, date = 1.3 },
+                new Data1Graph(){ values = 197.5, date = 1.4 },
+                new Data1Graph(){ values = 197.6, date = 1.5 },
+                new Data1Graph(){ values = 197.6, date = 1.6 },
+                new Data1Graph(){ values = 197.6, date = 1.7 },
+                new Data1Graph(){ values = 197.6, date = 1.8 },
+                new Data1Graph(){ values = 197.7, date = 1.9 },
+                new Data1Graph(){ values = 197.7, date = 2.0 },
+                new Data1Graph(){ values = 197.7, date = 2.1 },
+                new Data1Graph(){ values = 197.7, date = 2.2 },
+                new Data1Graph(){ values = 197.7, date = 2.3 },
+                new Data1Graph(){ values = 197.7, date = 2.4 },
+                new Data1Graph(){ values = 197.8, date = 2.5 },
+                new Data1Graph(){ values = 197.8, date = 2.6 },
+                new Data1Graph(){ values = 197.8, date = 2.7 },
+                new Data1Graph(){ values = 197.8, date = 2.8 },
+                new Data1Graph(){ values = 197.8, date = 2.9 },
+                new Data1Graph(){ values = 197.8, date = 3.0 },
+            };
+
+            datas2 = new List<Data2Graph>() // Массив с hv от G
+            {
+                new Data2Graph(){ values = 0.0005, date = 1.409 },
+                new Data2Graph(){ values = 0.0005, date = 1.512 },
+                new Data2Graph(){ values = 0.0039, date = 1.625 },
+                new Data2Graph(){ values = 0.0820, date = 1.722 },
+                new Data2Graph(){ values = 0.3333, date = 1.799 },
+                new Data2Graph(){ values = 0.6374, date = 1.873 },
+                new Data2Graph(){ values = 1.0000, date = 1.947 },
+                new Data2Graph(){ values = 0.8703, date = 2.023 },
+                new Data2Graph(){ values = 0.1492, date = 2.095 },
+                new Data2Graph(){ values = 0.1027, date = 2.164 },
+                new Data2Graph(){ values = 0.0697, date = 2.226 },
+                new Data2Graph(){ values = 0.0504, date = 2.292 },
+                new Data2Graph(){ values = 0.0347, date = 2.357 },
+                new Data2Graph(){ values = 0.0203, date = 2.431 },
+                new Data2Graph(){ values = 0.0139, date = 2.489 },
+                new Data2Graph(){ values = 0.0104, date = 2.546 },
+                new Data2Graph(){ values = 0.0087, date = 2.599 },
+                new Data2Graph(){ values = 0.0064, date = 2.649 },
+                new Data2Graph(){ values = 0.0072, date = 2.696 },
+            };
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            WidthSignal();
+        }
+        private void WidthSignal()
+        {
+            SeriesCollection series = new SeriesCollection();
+            ChartValues<double> zp = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<string> date = new List<string>(); //здесь будут храниться значения для оси X
+            foreach (var item in datas1)
+            {
+                zp.Add(item.values);
+                date.Add(item.date.ToString());
+            }
+            cartesianChart1.AxisX.Clear();
+            cartesianChart1.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "d, mm",
+                Labels = date
+            });
+            cartesianChart1.AxisY.Clear();
+            cartesianChart1.AxisY.Add(new Axis //Добавляем на ось Y значения, через блок инициализатора.
+            {
+                Title = "U, mV",
+            });
+
+            LineSeries line = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line.Title = "";
+            line.Values = zp;
+
+            series.Add(line); //Добавляем линию на график
+            cartesianChart1.Series = series; //Отрисовываем график в интерфейсе
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            PhotoconductivityDependence();
+        }
+
+        private void PhotoconductivityDependence()
+        {
+            SeriesCollection series = new SeriesCollection();
+            ChartValues<double> zp = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<string> date = new List<string>(); //Здесь будут храниться значения для оси X
+            foreach (var item in datas2)
+            {
+                zp.Add(item.values);
+                date.Add(item.date.ToString());
+            }
+            cartesianChart1.AxisX.Clear();
+            cartesianChart1.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "hv, эВ",
+                Labels = date
+            });
+            cartesianChart1.AxisY.Clear();
+            cartesianChart1.AxisY.Add(new Axis //Добавляем на ось Y значения, через блок инициализатора.
+            {
+                Title = "G, per-unit",
+            });
+
+            LineSeries line = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line.Title = "";
+            line.Values = zp;
+
+            series.Add(line); //Добавляем линию на график
+            
+            cartesianChart1.Series = series; //Отрисовываем график в интерфейсе
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+            int line = 0;
+            List<double> alpha = new List<double>() { 3300, 3200, 3100, 3000, 2900, 2800, 2700, 2600, 2500, 2400, 2300, 2200, 2100,
+            2000, 1900, 1800, 1700, 1600, 1500 };
+            List<double> L = new List<double>() { 0.880, 0.820, 0.763, 0.720, 0.689, 0.662, 0.637, 0.613, 0.592, 0.573, 0.557, 0.541, 
+            0.526, 0.510, 0.498, 0.487, 0.477, 0.468, 0.460 };
+            List<double> hv = new List<double>();
+            for (int i = 0; i < L.Count; i++)
+            {
+                hv.Add(1.24 / L[i]);
+            }
+            List<double> U = new List<double>() { 0.1, 0.1, 0.7, 13.2, 48.5, 84.3, 119.0, 91.8, 14.0, 8.5, 5.2, 3.3, 2.0, 1.0, 0.6, 0.4, 
+            0.3, 0.2, 0.2 };
+            List<double> I = new List<double>() { 0.766, 0.732, 0.657, 0.595, 0.538, 0.489, 0.440, 0.390, 0.347, 0.306, 0.276, 0.242, 
+            0.213, 0.182, 0.160, 0.142, 0.127, 0.115, 0.103 };
+            List<double> U_I = new List<double>();
+            double uMax = 0;
+            for (int i = 0; i < L.Count; i++)
+            {
+                U_I.Add(U[i] / I[i]);
+                if (uMax < U_I[i])
+                {
+                    uMax = U_I[i];
+                }
+            }
+            List<double> G = new List<double>();
+            for (int i = 0; i < L.Count; i++)
+            {
+                G.Add(U_I[i] / uMax);
+            }
+            for (int i = 0; i < alpha.Count; i++)
+            {
+                if (Math.Round(Convert.ToDouble(text)) == alpha[i])
+                {
+                    line = i;
+                    break;
+                }
+            }
+            if (Convert.ToInt32(text) <= 3300 && Convert.ToInt32(text) >= 1500 )
+            {
+                label2.Text = $"L = {L[line].ToString()} μm";
+                label3.Text = $"hv = {Math.Round(hv[line], 6).ToString()} eV";
+                label4.Text = $"U = {U[line].ToString()} mV";
+                label5.Text = $"U/I = {Math.Round(U_I[line], 6).ToString()}^10e-3 per-unit";
+                label6.Text = $"G = {Math.Round(G[line], 6).ToString()} per-unit";
+                label7.Text = "Valid values are [1500; 3300]";
+            }
+            else
+            {
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "Please enter a number \nbetween 1500 and 3300.";
+            }
+        }
+
+        private void textBox1_1(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // можно нажимать только цифры и клавишн BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
